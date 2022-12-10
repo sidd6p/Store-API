6a875d2a7bd1751bfc53ea11008bb68b1cdc93ee
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-10 16:26:02 +0530

    Bug Fix: Standard error msg for duplicate store and JWT required implemeted for store

--- resources/store.py
+++ resources/store.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from flask_jwt_extended import jwt_required
 
 from db import db
 from schemas import StoreSchema
@@ -14,23 +15,24 @@ class StoreList(MethodView):
     def get(self):
         return StoreModel.query.all()
 
+    @jwt_required()
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
         store = StoreModel(**store_data)
-        # try:
-        db.session.add(store)
-        db.session.commit()
-        # except IntegrityError:
-        #     abort(
-        #         400,
-        #         message="Store with same name already exists"
-        #     )
-        # except SQLAlchemyError:
-        #     abort(
-        #         500, 
-        #         message="Error while inserting item"
-        #     )
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="Store with same name already exists"
+            )
+        except SQLAlchemyError:
+            abort(
+                500, 
+                message="Error while inserting item"
+            )
         return store
 
 
@@ -47,6 +49,7 @@ class Store(MethodView):
                 "Store Not found"
             )
 
+    @jwt_required()
     @blp.response(200, StoreSchema)
     def delete(self, store_id):
         try:
