7bcccdf7482d72dfc6476f362936ebace5de5655
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-10 13:53:45 +0530

    added user model

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.item import ItemModel
 from models.store import StoreModel
 from models.tag import TagModel
 from models.item_tag import ItemTagModel
+from models.user import UserModel
---
+++ models/user.py
@@ -0,0 +1,8 @@
+from db import db
+
+class UserModel(db.Models):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
