dc9ba99e6bcfff3f07bd88146a91a8966085a749
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-10 13:56:51 +0530

    updated user model with required=True for usernmae

--- schemas.py
+++ schemas.py
@@ -16,7 +16,7 @@ class PlainTagSchema(Schema):
 class PlainUserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
-    password = fields.str(load_only=True)
+    password = fields.str(required=True, load_only=True)
 
 
 
