04ee6e9c301897c3776da46fb8c56dd3aed1fa4f
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-11 17:28:09 +0530

    Built: added new column in items table using migrate

--- models/item.py
+++ models/item.py
@@ -7,6 +7,7 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"),nullable=False)
-    
+    description= db.Column(db.String(200))
+
     store = db.relationship("StoreModel", back_populates="item")
     tags = db.relationship("TagModel", back_populates='items', secondary="tags_in_items")
